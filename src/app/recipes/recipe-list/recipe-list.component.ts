5152b4582fc39d10be9bc2aa9b7e4b0d534ed421
Vasileios Argyroglou <b.argyroglou@gmail.com>
2020-12-30 18:33:12 +0200

    Section 3 full implementation

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe' , 'This is a simple test' , 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.wNk0znq6E1ysLCNHNSQUzgHaLG%26pid%3DApi&f=1'),
+    new Recipe('A Test Recipe' , 'This is a simple test' , 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.wNk0znq6E1ysLCNHNSQUzgHaLG%26pid%3DApi&f=1')
+  ];
 
   constructor() { }
 
