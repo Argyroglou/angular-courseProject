d0d080aa909fa3cbfa8bb625b884667173ed6145
Vasileios Argyroglou <b.argyroglou@gmail.com>
2021-01-02 12:02:34 +0200

    Section 6.88

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,9 +7,11 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe' , 'This is a simple test' , 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.wNk0znq6E1ysLCNHNSQUzgHaLG%26pid%3DApi&f=1'),
-    new Recipe('A Test Recipe' , 'This is a simple test' , 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.wNk0znq6E1ysLCNHNSQUzgHaLG%26pid%3DApi&f=1')
+    new Recipe('A Test Recipe', 'This is a simple test', 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.wNk0znq6E1ysLCNHNSQUzgHaLG%26pid%3DApi&f=1'),
+    new Recipe('A Another Test Recipe', 'This is a simple test', 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.wNk0znq6E1ysLCNHNSQUzgHaLG%26pid%3DApi&f=1')
   ];
 
   constructor() { }
@@ -17,4 +19,8 @@ export class RecipeListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
