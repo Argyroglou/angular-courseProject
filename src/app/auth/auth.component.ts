09ef375f8d5cf8e485b096a4b0b1cf56eb4aa170
Vasileios Argyroglou <b.argyroglou@gmail.com>
2021-01-26 20:08:20 +0200

    Loggin error handling

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,18 +1,19 @@
-import { Component } from "@angular/core";
-import { NgForm } from "@angular/forms";
-import { AuthService } from "./auth.service";
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Observable } from 'rxjs';
+
+import { AuthService, AuthResponseData } from './auth.service';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html'
 })
 export class AuthComponent {
-  isLoginMode: boolean = true;
-  isLoading: boolean = false;
+  isLoginMode = true;
+  isLoading = false;
   error: string = null;
 
-
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService) {}
 
   onSwitchMode() {
     this.isLoginMode = !this.isLoginMode;
@@ -25,21 +26,28 @@ export class AuthComponent {
     const email = form.value.email;
     const password = form.value.password;
 
+    let authObs: Observable<AuthResponseData>;
+
     this.isLoading = true;
+
     if (this.isLoginMode) {
-      //..
+      authObs = this.authService.login(email, password);
     } else {
-      this.authService.signup(email, password).subscribe(resData => {
+      authObs = this.authService.signup(email, password);
+    }
+
+    authObs.subscribe(
+      resData => {
         console.log(resData);
         this.isLoading = false;
       },
-        errorMessage => {
-          console.log(errorMessage);
-          this.error = errorMessage;
-          this.isLoading = false;
-        });
-      form.reset();
-    }
+      errorMessage => {
+        console.log(errorMessage);
+        this.error = errorMessage;
+        this.isLoading = false;
+      }
+    );
+
+    form.reset();
   }
 }
-
